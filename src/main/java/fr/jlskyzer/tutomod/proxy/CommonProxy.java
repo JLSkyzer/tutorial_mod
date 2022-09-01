f8840519c7cdaf68f5f516ec6a33a43b8cefcc80
JLSkyzer <killiandasneves4@gmail.com>
2022-09-01 14:37:39 +0200

    First commit

---
+++ src/main/java/fr/jlskyzer/tutomod/proxy/CommonProxy.java
@@ -0,0 +1,7 @@
+package fr.jlskyzer.tutomod.proxy;
+
+import net.minecraft.item.Item;
+
+public abstract class CommonProxy {
+    public void registerModel(Item item, int metadata) {}
+}
---
+++ src/main/java/fr/jlskyzer/tutomod/util/Reference.java
@@ -0,0 +1,11 @@
+package fr.jlskyzer.tutomod.util;
+
+public class Reference {
+
+    public static final String MODID = "tutorial";
+    public static final String NAME = "Tutorial Mod";
+    public static final String VERSION = "1.12.2";
+
+    public static final String CLIENT =  "fr.jlskyzer.tutomod.proxy.ClientProxy";
+    public static final String COMMON =  "fr.jlskyzer.tutomod.proxy.CommonProxy";
+}
